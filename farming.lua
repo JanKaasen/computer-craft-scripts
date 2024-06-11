8b67df20ddabcb44580cb63a78abddfe4db35a49
Jan Trygve Kaasen <jtkaasen@gmail.com>
2024-06-11 23:15:03 +0200

    Updated farming script

--- farming.lua
+++ farming.lua
@@ -11,7 +11,7 @@
 function Refuel()
   for i = 1, 16, 1 do
     local itemData = turtle.getItemDetail()
-    if itemData.name == "minecraft:coal" or "minecraft:charcoal" then
+    if itemData and itemData.name == "minecraft:coal" or "minecraft:charcoal" then
       turtle.select(i)
       turtle.refuel()
     end
@@ -19,18 +19,21 @@ function Refuel()
 end
 
 function CheckGrowth()
-  local cropData = turtle.inspectDown()
-  if cropData.state.age == 7 then
+  local success, cropData = turtle.inspectDown()
+  if success and cropData.state.age == 7 then
     turtle.digDown()
+  elseif not success then
+    turtle.forward()
   end
 end
 
 function PlantSeed()
-  for i = 1, 16, 1 do
+  for i = 1, 16 do
     local itemData = turtle.getItemDetail(i)
-    if itemData.name == "minecraft:wheat_seeds" then
+    if itemData and itemData.name == "minecraft:wheat_seeds" then
       turtle.select(i)
       turtle.placeDown()
+      return
     end
   end
 end
@@ -39,3 +42,54 @@ function HarvestAndPlant()
   CheckGrowth()
   PlantSeed()
 end
+
+function MoveForward()
+  turtle.forward()
+  HarvestAndPlant()
+end
+
+function TurnRight()
+  turtle.turnRight()
+  turtle.forward()
+  HarvestAndPlant()
+  turtle.turnRight()
+end
+
+function TurnLeft()
+  turtle.turnLeft()
+  turtle.forward()
+  HarvestAndPlant()
+  turtle.turnLeft()
+end
+
+function FarmField()
+  local width = 8
+  local length = 9
+
+  for y = 1, length do
+    for x = 1, width do
+      MoveForward()
+    end
+    if y < length then
+      if y % 2 == 1 then
+        TurnRight()
+      else
+        TurnLeft()
+      end
+    end
+  end
+end
+
+function ReturnHome()
+  print(X, Y)
+end
+
+function HonestWork()
+  Refuel()
+  CheckGrowth()
+  PlantSeed()
+  FarmField()
+  ReturnHome()
+end
+
+HonestWork()
